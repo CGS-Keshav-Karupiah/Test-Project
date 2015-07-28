e2ce361f358fd2bfb7bedbd539b5948e5cabff04
Keshav Karupiah <2333@cgs.act.edu.au>
2015-07-28 21:35:18 +1000

    Initialised master commit

---
+++ another_Test/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  another_Test
+//
+//  Created by Keshav Karupiah on 28/07/2015.
+//  Copyright (c) 2015 Keshav Karupiah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("Initialised")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
